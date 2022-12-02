9ba01a1f5c780e7910257c3867e0d0aac10ac6a2
Daniel Bachmann <dba@macmini.local>
2022-12-02 16:52:10 +0100

    initial add

---
+++ search.js
@@ -0,0 +1,59 @@
+const ra = require('remove-accents');
+const fs = require('fs');
+const cliProgress = require('cli-progress');
+const fnames = [ 'GerTai.xml', 'FrozenKernel.xml' ];
+
+for (var i=0; i<fnames.length; i++) {
+  doSearch(fnames[i]);
+}
+
+function doSearch(fname) {
+  //const fname = 'Bad Robert.json';
+  const target = fs.readFileSync(fname, 'utf-8');
+  let tLines = null;
+  tLines = target.split(/\r?\n/);
+
+  let fo = null;
+  /*if (fname.match(/\.json$/ig)) {
+    fo = JSON.parse(target);
+    for (var i=0; i<10; i++) {
+      console.log(fo[i], typeof(fo[i]));
+    }
+  } else {
+    tLines = target.split(/\r?\n/);
+  }*/
+  const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  fs.readFile('wishlist', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const dlines = data.split(/\r?\n/).filter( n => n);
+    console.log(`search ${fname} pending ...`);
+    bar.start(dlines.length, 0);
+    dlines.forEach(line =>  {
+      bar.increment();
+      // console.log(`check line: ${line}`);
+      const words = line.split(' ').filter(n => n);
+      //if (fname.match(/\.xml$/ig)) {
+        for (const tline in tLines) {
+          if (matchAll(words, tLines[tline])) {
+             console.log('\rfound '+line+' in \n', tLines[tline]);
+          } 
+        }
+      /*} else if (fname.match(/\.json$/ig)) {
+      }*/
+    });
+    bar.stop();
+    console.log('search done\n');
+  });
+}
+
+function matchAll(arr, line) {
+  const l = ra.remove(line).toLowerCase();
+  for (var i=0; i<arr.length; i++) {
+    const re = new RegExp(ra.remove(arr[i]).toLowerCase(), 'gi');
+    if (!l.match(re)) return false;
+  }
+  return true;
+}
