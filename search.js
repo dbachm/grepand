71e0da59b647d57818ab4e1200f0b874428a44da
Daniel Bachmann <dba@macmini.local>
2022-12-02 18:38:07 +0100

    use tee for pipe into file

--- search.js
+++ search.js
@@ -1,37 +1,20 @@
 const ra = require('remove-accents');
 const fs = require('fs');
-// overload cosole.log to log file
 const util = require('util');
-const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
-const log_stdout = process.stdout;
-
-console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
-};
 const cliProgress = require('cli-progress');
 //const fnames = [ 'GerTai.xml', 'FrozenKernel.xml' ];
-const fnames = [ 'gasby69154.json' ];
+const fnames = [ 'Bad Robert.json', 'gasby69154.json' ];
 
 for (var i=0; i<fnames.length; i++) {
   doSearch(fnames[i]);
 }
 
 function doSearch(fname) {
-  //const fname = 'Bad Robert.json';
   const target = fs.readFileSync(fname, 'utf-8');
   let tLines = null;
   tLines = target.split(/\r?\n/);
 
   let fo = null;
-  /*if (fname.match(/\.json$/ig)) {
-    fo = JSON.parse(target);
-    for (var i=0; i<10; i++) {
-      console.log(fo[i], typeof(fo[i]));
-    }
-  } else {
-    tLines = target.split(/\r?\n/);
-  }*/
   const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
   fs.readFile('wishlist', 'utf8', (err, data) => {
     if (err) {
@@ -43,16 +26,13 @@ function doSearch(fname) {
     bar.start(dlines.length, 0);
     dlines.forEach(line =>  {
       bar.increment();
-      // console.log(`check line: ${line}`);
       const words = line.split(' ').filter(n => n);
       //if (fname.match(/\.xml$/ig)) {
         for (const tline in tLines) {
           if (matchAll(words, tLines[tline])) {
-             console.log('\rfound '+line+' in \n', tLines[tline]);
+             console.log('\rfound <'+line+'> in ', tLines[tline]);
           } 
         }
-      /*} else if (fname.match(/\.json$/ig)) {
-      }*/
     });
     bar.stop();
     console.log('search done\n');
