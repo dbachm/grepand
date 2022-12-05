a5d072119bb323377d663e6c15d1c2d7411ca518
Daniel Bachmann <dba@macmini.local>
2022-12-06 00:10:48 +0100

    only read wishlist once (not with every file)

--- search.js
+++ search.js
@@ -2,12 +2,21 @@ const ra = require('remove-accents');
 const fs = require('fs');
 const util = require('util');
 const cliProgress = require('cli-progress');
-
-const targetFolder = './target/';
-console.log(new Date().toISOString().replace('T',' ').substring(0,19), 'search started');
-fs.readdirSync(targetFolder).forEach(file => {
-  doSearch(targetFolder+file);
+let dlines;
+fs.readFile('wishlist', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    dlines = data.split(/\r?\n/).filter( n => n);
+    console.log(dlines);
+    const targetFolder = './target/';
+    console.log(new Date().toISOString().replace('T',' ').substring(0,19), 'search started');
+    fs.readdirSync(targetFolder).forEach(file => {
+      doSearch(targetFolder+file);
+    });
 });
+
 
 function doSearch(fname) {
   const target = fs.readFileSync(fname, 'utf-8');
@@ -15,27 +24,20 @@ function doSearch(fname) {
   tLines = target.split(/\r?\n/).filter( n => n.length > 10);
   let fo = null;
   const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-  fs.readFile('wishlist', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const dlines = data.split(/\r?\n/).filter( n => n);
-    console.log(`search ${fname} pending ...`);
-    bar.start(dlines.length, 0);
-    dlines.forEach(line =>  {
-      bar.increment();
-      const words = line.split(' ').filter(n => n);
-      //if (fname.match(/\.xml$/ig)) {
-        for (const tline in tLines) {
-          if (matchAll(words, tLines[tline])) {
-             console.log('\rfound <'+line+'> in ', tLines[tline]);
-          } 
-        }
-    });
-    bar.stop();
-    console.log('search done\n');
+  console.log(`search ${fname} pending ...`);
+  bar.start(dlines.length, 0);
+  dlines.forEach(line =>  {
+    bar.increment();
+    const words = line.split(' ').filter(n => n);
+    //if (fname.match(/\.xml$/ig)) {
+      for (const tline in tLines) {
+        if (matchAll(words, tLines[tline])) {
+           console.log('\rfound <'+line+'> in ', tLines[tline]);
+        } 
+      }
   });
+  bar.stop();
+  console.log('search done\n');
 }
 
 function matchAll(arr, line) {
