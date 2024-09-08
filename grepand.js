fc0df9afe5da9650eba44b691c2cd71dd20f45c4
Daniel Bachmann <dba@macmini.local>
2024-09-08 12:01:23 +0200

    log local date (instead of utc)

--- grepand.js
+++ grepand.js
@@ -10,7 +10,24 @@ fs.readFile('wishlist', 'utf8', (err, data) => {
     }
     dlines = data.split(/\r?\n/).filter( n => n);
     const targetFolder = './target/';
-    console.log(new Date().toISOString().replace('T',' ').substring(0,19), 'search started');
+    function toISOLocal(d) {
+      var z  = n =>  ('0' + n).slice(-2);
+      var zz = n => ('00' + n).slice(-3);
+      var off = d.getTimezoneOffset();
+      var sign = off > 0? '-' : '+';
+      off = Math.abs(off);
+
+      return d.getFullYear() + '-'
+             + z(d.getMonth()+1) + '-' +
+             z(d.getDate()) + 'T' +
+             z(d.getHours()) + ':'  + 
+             z(d.getMinutes()) + ':' +
+             z(d.getSeconds()) + '.' +
+             zz(d.getMilliseconds()) +
+             sign + z(off/60|0) + ':' + z(off%60); 
+    }
+
+    console.log(toISOLocal(new Date()).replace('T',' ').substring(0,19), 'search started');
     fs.readdirSync(targetFolder).forEach(file => {
       doSearch(targetFolder+file);
     });
